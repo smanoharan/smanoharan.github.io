cd53f85b32c60bd3ddadc8ef2e68b6929a878a38
Siva Manoharan <smanoharan@outlook.com>
2020-02-10 22:37:16 +1300

    Updated to relative path

--- three-react/service-worker.js
+++ three-react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c47e598227466d5553bb2264878c1e50.js"
+  "./precache-manifest.f78ba58d7f33e573cf4b5fc5fa9d16a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
