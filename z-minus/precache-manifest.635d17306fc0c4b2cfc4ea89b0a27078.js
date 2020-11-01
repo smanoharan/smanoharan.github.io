9468fadbaf60928d0d31579e8ae192916874a371
Siva <smanoharan@outlook.com>
2020-11-01 17:19:58 +1300

    Updated z-minus

---
+++ z-minus/precache-manifest.635d17306fc0c4b2cfc4ea89b0a27078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff95914fc6d7e4a72084f44c4fe6b30",
+    "url": "/z-minus/index.html"
+  },
+  {
+    "revision": "8c96db0dd74ce8cc623c",
+    "url": "/z-minus/static/css/main.36105d61.chunk.css"
+  },
+  {
+    "revision": "39cdcbe68b06d839a591",
+    "url": "/z-minus/static/js/2.303554f4.chunk.js"
+  },
+  {
+    "revision": "b3e2770e6336acd821638b70a2a55e3e",
+    "url": "/z-minus/static/js/2.303554f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c96db0dd74ce8cc623c",
+    "url": "/z-minus/static/js/main.85fb2673.chunk.js"
+  },
+  {
+    "revision": "206ff1c79f5827cbc5db",
+    "url": "/z-minus/static/js/runtime-main.14ae362f.js"
+  }
+]);
