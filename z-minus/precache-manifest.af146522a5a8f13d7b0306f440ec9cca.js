74c3bf8f1aa8ddc86850a7a7a461a7ad5093b28c
Siva <smanoharan@outlook.com>
2020-11-21 17:42:37 +1300

    Updating Z-Minus

---
+++ z-minus/precache-manifest.af146522a5a8f13d7b0306f440ec9cca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26cc35f7d2bf13e32f4ba18132bcf9e",
+    "url": "/z-minus/9fa53c7cc47b56e7e4ce.worker.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/z-minus/9fa53c7cc47b56e7e4ce.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "6260ce37f6a6ee0351d2b7b08ddc9f95",
+    "url": "/z-minus/index.html"
+  },
+  {
+    "revision": "40fd4b1f6d59477255ec",
+    "url": "/z-minus/static/css/main.2cb1fee5.chunk.css"
+  },
+  {
+    "revision": "e1fd8d161d2cbbeb9270",
+    "url": "/z-minus/static/js/2.2d79f2a8.chunk.js"
+  },
+  {
+    "revision": "4f85414e92c823203461bb7d5ec7cad7",
+    "url": "/z-minus/static/js/2.2d79f2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40fd4b1f6d59477255ec",
+    "url": "/z-minus/static/js/main.37075e21.chunk.js"
+  },
+  {
+    "revision": "206ff1c79f5827cbc5db",
+    "url": "/z-minus/static/js/runtime-main.14ae362f.js"
+  }
+]);
